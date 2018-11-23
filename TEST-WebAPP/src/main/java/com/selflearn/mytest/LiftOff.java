02e6d9eaae95d9efcb8e7b57532c4a243e935354
szw <szw3366@126.com>
2018-11-23 09:59:20 +0800

    Initial commit

---
+++ TEST-WebAPP/src/main/java/com/selflearn/mytest/LiftOff.java
@@ -0,0 +1,14 @@
+package com.selflearn.mytest;
+
+/**
+ * @author clz
+ * @date 2018/11/22 18:04
+ */
+public class LiftOff implements Runnable {
+    protected int countDown = 10;
+
+    @Override
+    public void run() {
+
+    }
+}
