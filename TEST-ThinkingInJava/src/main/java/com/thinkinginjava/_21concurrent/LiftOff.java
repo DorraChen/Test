f57a774e4d4da92f1c322ec6d1ebf99145ec0455
szw <szw3366@126.com>
2018-11-23 10:28:36 +0800

    11/23 <java 编程思想>21章-并发,了解线程

--- TEST-ThinkingInJava/src/main/java/com/thinkinginjava/_21concurrent/LiftOff.java
+++ TEST-ThinkingInJava/src/main/java/com/thinkinginjava/_21concurrent/LiftOff.java
@@ -5,10 +5,33 @@ package com.thinkinginjava._21concurrent;
  * @date 2018/11/22 18:04
  */
 public class LiftOff implements Runnable {
+    /**
+     * 10次循环
+     */
     protected int countDown = 10;
+
+    /**
+     * 计数器初始值
+     */
+    private static int taskCount = 0;
+    /**
+     * 计数器
+     */
+    private final int id = taskCount++;
+
+    /**
+     * 打印的内容
+     * @return
+     */
+    public String status() {
+        return "#" + id + "(" + (countDown > 0 ? countDown : "LiftOff") + "), ";
+    }
 
     @Override
     public void run() {
-
+        while (countDown-- > 0){
+            System.out.print(status());
+            Thread.yield();
+        }
     }
 }
---
+++ TEST-ThinkingInJava/src/main/java/com/thinkinginjava/_21concurrent/MainThread.java
@@ -0,0 +1,8 @@
+package com.thinkinginjava._21concurrent;
+
+/**
+ * @author clz
+ * @date 2018/11/23 10:26
+ */
+public class MainThread {
+}
