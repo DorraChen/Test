aa1f6d7768a6447cd037fefc7d89234aa03e4e88
chenlizhen <1814786186@qq.com>
2020-09-04 18:06:31 +0800

    多线程学习之Fork/Join

--- TEST-Thread/src/main/java/com/forkjoin/ForkJoinTest.java
+++ TEST-Thread/src/main/java/com/forkjoin/ForkJoinTest.java
@@ -1,5 +1,10 @@
 package com.forkjoin;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
 /**
  * @author clz
  * @data 2020/9/4 15:40
@@ -7,5 +12,80 @@ package com.forkjoin;
  */
 public class ForkJoinTest {
     public static void main(String[] args) {
+        String[] fc = {"hello world",
+                "hello me",
+                "hello fork",
+                "hello join",
+                "fork join in world"};
+        // 创建 ForkJoin 线程池
+        ForkJoinPool fjp = new ForkJoinPool(3);
+        // 创建任务
+        MapReduce mr = new MapReduce(fc, 0, fc.length);
+        // 启动任务
+        Map<String, Long> result = fjp.invoke(mr);
+        // 输出结果
+        result.forEach((k, v) -> System.out.println(k + ":" + v));
+    }
+
+    /**
+     * MapReduce 模拟类
+     */
+    static class MapReduce extends RecursiveTask<Map<String, Long>> {
+        private String[] fc;
+        private int start, end;
+
+        // 构造函数
+        MapReduce(String[] fc, int fr, int to) {
+            this.fc = fc;
+            this.start = fr;
+            this.end = to;
+        }
+
+        @Override
+        protected Map<String, Long> compute() {
+            if (end - start == 1) {
+                return calc(fc[start]);
+            } else {
+                int mid = (start + end) / 2;
+                MapReduce mr1 = new MapReduce(fc, start, mid);
+                mr1.fork();
+                MapReduce mr2 = new MapReduce(fc, mid, end);
+                // 计算子任务，并返回合并的结果
+                return merge(mr2.compute(), mr1.join());
+            }
+        }
+
+        // 合并结果
+        private Map<String, Long> merge(Map<String, Long> r1, Map<String, Long> r2) {
+            Map<String, Long> result = new HashMap<>(16);
+            result.putAll(r1);
+            // 合并结果
+            r2.forEach((k, v) -> {
+                Long c = result.get(k);
+                if (c != null) {
+                    result.put(k, c + v);
+                } else {
+                    result.put(k, v);
+                }
+            });
+            return result;
+        }
+
+        // 统计单词数量
+        private Map<String, Long> calc(String line) {
+            Map<String, Long> result = new HashMap<>(16);
+            // 分割单词
+            String[] words = line.split("\\s+");
+            // 统计单词数量
+            for (String w : words) {
+                Long v = result.get(w);
+                if (v != null) {
+                    result.put(w, v + 1);
+                } else {
+                    result.put(w, 1L);
+                }
+            }
+            return result;
+        }
     }
 }
